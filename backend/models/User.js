93c6c97a495afec22873bf786194bd038afd94ad
Suhail Malik <devmsuhail@gmail.com>
2020-11-04 14:51:17 +0530

    Added login validated otp and token generation

---
+++ backend/models/User.js
@@ -0,0 +1,55 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  phone: {
+    type: String,
+    min: 10,
+    max: 10,
+    required: true,
+  },
+  addresses: [
+    {
+      surname: {
+        type: String,
+        min: 2,
+        max: 2,
+        required: true,
+      },
+      name: {
+        type: String,
+        min: 2,
+        max: 255,
+        required: true,
+      },
+      flat: {
+        type: String,
+        min: 2,
+        max: 255,
+        required: true,
+      },
+      street: {
+        type: String,
+        min: 2,
+        max: 255,
+        required: true,
+      },
+      location: {
+        type: {
+          type: String,
+          default: "Point",
+        },
+        coordinates: [Number], // [22.2475, 14.2547]  [longitude, latitude]
+      },
+      addressType: {
+        type: String,
+        min: 1,
+        max: 1,
+        required: true,
+      },
+    },
+  ],
+});
+
+module.exports = mongoose.model("User", userSchema);
