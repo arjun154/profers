47ce2e21e5f40823aee61ff49dbbc9c4830b9cea
Suhail Malik <devmsuhail@gmail.com>
2020-11-05 11:37:59 +0530

    Backend: Enabled cors for everydomain

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const cors = require("cors");
 const userRoutes = require("./routes/userRoutes");
 
 const app = express();
@@ -16,6 +17,7 @@ mongoose.connect(
   }
 );
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/api/V1/accounts", userRoutes);
