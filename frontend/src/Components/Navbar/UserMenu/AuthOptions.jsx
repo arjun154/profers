8c70da05c72eacd5d1fe063bf2a61927b07ab6fc
gouthamikr <gouthamikyatham4@gmail.com>
2020-11-09 22:23:31 +0530

    after login dropdown created and address modal css updated

---
+++ frontend/src/Components/Navbar/UserMenu/AuthOptions.jsx
@@ -0,0 +1,102 @@
+import React from "react";
+import { ListItemIcon, ListItemText } from "@material-ui/core";
+import StyledMenuItem from "../../Common/StyledMenuItem";
+import { Link } from "react-router-dom";
+import Styles from "../../MyOrders/styles.module.css";
+
+const UnAuthOptions = ({ openLoginModal }) => {
+  const [phoneNumber] = React.useState("8279880948");
+  return (
+    <>
+      <StyledMenuItem>
+        <ListItemIcon>
+          <img
+            src="https://grofers.com/images/header/user-profile-667209e.png"
+            alt="accountLogo"
+            width="50px"
+          />
+        </ListItemIcon>
+        <ListItemText>
+          <small>+91 {phoneNumber}</small>
+        </ListItemText>
+      </StyledMenuItem>
+
+      <StyledMenuItem>
+        <ListItemIcon>
+          <i class="fal fa-business-time"></i>
+        </ListItemIcon>
+        <ListItemText>
+          <Link
+            to="/account/orders"
+            className={Styles.link}
+            style={{ color: "gray" }}
+          >
+            <div style={{ marginTop: "10%" }}>My Orders</div>
+
+            <small>View past orders / Report an issue</small>
+          </Link>
+        </ListItemText>
+      </StyledMenuItem>
+
+      <StyledMenuItem>
+        <ListItemIcon>
+          <i class="fal fa-map-marked-alt"></i>
+        </ListItemIcon>
+        <ListItemText>
+          <Link
+            to="/account/addresses"
+            className={Styles.link}
+            style={{ color: "gray" }}
+          >
+            My Addresses
+          </Link>
+        </ListItemText>
+      </StyledMenuItem>
+
+      <StyledMenuItem>
+        <ListItemIcon>
+          <i
+            class="fal fa-rupee-sign"
+            style={{
+              border: "1px solid grey",
+              padding: "2px 5px",
+              borderRadius: "50%",
+            }}
+          ></i>{" "}
+        </ListItemIcon>
+        <ListItemText style={{ color: "gray" }}>My Wallet</ListItemText>
+      </StyledMenuItem>
+
+      <StyledMenuItem>
+        <ListItemIcon>
+          <i className="far fa-question-circle"></i>
+        </ListItemIcon>
+        <ListItemText primary="FAQs" style={{ color: "gray" }} />
+      </StyledMenuItem>
+
+      <StyledMenuItem>
+        <ListItemIcon>
+          <i className="fas fa-percent"></i>
+        </ListItemIcon>
+        <ListItemText primary="Offers" style={{ color: "gray" }} />
+      </StyledMenuItem>
+
+      <StyledMenuItem>
+        <ListItemIcon>
+          <i class="fal fa-user"></i>
+        </ListItemIcon>
+        <ListItemText style={{ color: "gray" }}>Logout</ListItemText>
+      </StyledMenuItem>
+
+      <StyledMenuItem>
+        <img
+          src="https://grofers.com/images/header/grofers-recipe-3ed3e37.png"
+          alt="recipe"
+          height="30px"
+        />
+      </StyledMenuItem>
+    </>
+  );
+};
+
+export default UnAuthOptions;
