eb3951aaa1c1fa46049fdbda0b6b7fb3b596273e
Suhail Malik <devmsuhail@gmail.com>
2020-11-10 11:04:08 +0530

    Front-End: Fixed Cart styling

--- frontend/src/Components/Cart/Cart.jsx
+++ frontend/src/Components/Cart/Cart.jsx
@@ -1,44 +1,62 @@
 import React from "react";
 import clsx from "clsx";
 import Styles from "./style.module.css";
-import { makeStyles } from "@material-ui/core";
+import { Box, makeStyles } from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
-  list: {
+  root: {
     width: 450,
+    display: "flex",
+    flexDirection: "column",
+    height: "100%",
   },
-  fullList: {
-    width: "auto",
+  cardMiddle: {
+    padding: "0.5rem  1rem",
+    display: "flex",
+    flexDirection: "column",
+    textAlign: "center",
+    alignItems: "center",
+    justifyContent: "center",
+    flex: 1,
+    alignSelf: "stretch",
+
+    "& > img": {
+      margin: "1rem 0",
+    },
   },
 }));
 
 const Cart = ({ toggleDrawer }) => {
   const classes = useStyles();
   return (
-    <div
-      className={clsx(classes.list, {
-        [classes.fullList]: true,
-      })}
-      role="presentation"
-      onClick={toggleDrawer}
-      onKeyDown={toggleDrawer}
-    >
+    <div className={classes.root} role="presentation">
       <div className={Styles.cartHeader}>
-        <div style={{ float: "left" }}>My Cart</div>
-        <div className={Styles.closeBtn}>×</div>
+        <div>My Cart</div>
+        <div
+          onClick={toggleDrawer}
+          onKeyDown={toggleDrawer}
+          className={Styles.closeBtn}
+        >
+          ×
+        </div>
       </div>
-      <img
-        src="https://grofers.com/images/cart/empty-cart_2x-da3645a.png"
-        alt="img"
-        className={Styles.cartLogo}
-      />
-      <div className={Styles.itemsMsg}> No items in your cart</div>
-      <div className={Styles.helperMsg}>
-        Your favourite items are just a click away
-      </div>
-      <div>
+
+      <Box className={classes.cardMiddle}>
+        {/* If no items then this div */}
+        <img
+          src="https://grofers.com/images/cart/empty-cart_2x-da3645a.png"
+          alt="img"
+          className={Styles.cartLogo}
+        />
+        <div className={Styles.itemsMsg}> No items in your cart</div>
+        <div className={Styles.helperMsg}>
+          Your favourite items are just a click away
+        </div>
+      </Box>
+
+      <Box textAlign="center" padding="0.5rem 0">
         <button className={Styles.btnShop}>Start Shopping</button>
-      </div>
+      </Box>
     </div>
   );
 };
