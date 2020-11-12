f96c517d208987f52371b2b29aae2d5abb9e1388
Suhail Malik <suhailmalik@Administrators-MacBook-Pro.local>
2020-11-12 18:12:42 +0530

    Cart will show price by default

--- frontend/src/Components/Cart/Icon.jsx
+++ frontend/src/Components/Cart/Icon.jsx
@@ -1,10 +1,14 @@
 import { makeStyles } from "@material-ui/core";
 import React from "react";
+import { useSelector } from "react-redux";
 
 const useStyels = makeStyles((theme) => ({
   text: {
-    height: 15,
-    width: 15,
+    height: 17,
+    width: 17,
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
     fontWeight: 600,
     borderRadius: "50%",
     position: "absolute",
@@ -18,14 +22,28 @@ const useStyels = makeStyles((theme) => ({
 
 const Icon = ({ onClick, className, count }) => {
   const classes = useStyels();
+
+  const { items } = useSelector((state) => state.cart);
+
+  const subTotal = Object.keys(items)
+    .map((key) => {
+      const totalPrice = items[key].varieties[0].price * items[key].qty;
+      const totalSale = (totalPrice * items[key].varieties[0].sale) / 100;
+      return totalPrice - totalSale;
+    })
+    .reduce((a, c) => a + c, 0);
+
   return (
     <div
       className={className}
       style={{ position: "relative" }}
       onClick={onClick}
     >
-      <i className="far fa-shopping-cart" style={{ marginRight: 20 }}></i>
-      Cart
+      <i
+        className="far fa-shopping-cart"
+        style={subTotal ? { marginRight: 20 } : {}}
+      ></i>
+      {subTotal ? <>₹{subTotal}</> : "Cart"}
       {count > 0 && <div className={classes.text}>{count}</div>}
     </div>
   );
