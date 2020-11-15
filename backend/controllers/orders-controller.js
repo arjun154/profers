896f92deb7ea8a341038c1db8c11c0e7a00f7a23
Suhail Malik <suhailmalik@Administrators-MacBook-Pro.local>
2020-11-15 23:22:43 +0530

    Added razor pay

--- backend/controllers/orders-controller.js
+++ backend/controllers/orders-controller.js
@@ -2,6 +2,16 @@ const Joi = require("joi")
 const Item = require("../models/Item")
 const User = require("../models/User")
 
+const { v4: uuid } = require('uuid')
+const RazorPay = require('razorpay')
+const { default: Axios } = require('axios')
+
+const instance = new RazorPay({
+    key_id: process.env.RAZOR_PAY_KEY_ID,
+    key_secret: process.env.RAZOR_PAY_KEY_SECRET
+})
+
+
 
 const getAllOrders = async (req, res) => {
     const { user } = req
@@ -15,11 +25,12 @@ const getAllOrders = async (req, res) => {
 }
 
 
+
 const validateOrder = data => {
     const schema = Joi.object({
         items: Joi.array().required().min(1).max(10),
         address: Joi.object().required(),
-        slot: Joi.date().required().min(new Date())
+        slot: Joi.date().min(new Date())
     })
 
     return schema.validate(data).error
@@ -27,16 +38,16 @@ const validateOrder = data => {
 
 const order = async (req, res) => {
     const { user } = req
-
+    let { price = 1000 } = req.query
     /* 
 
     headers
-    ------------
+    ------------------------------------------
     should conatain auth token as bearer token
     ------------------------------------------
 
     will accept in body
-    ------------------------
+    -----------------------------------------------
     - items.arrat()     => which will contain only {_id, ...etc of the item}
     - address.Object    => which will be only address from users addresses
     - slot.date()       => which will be the date when user wants the order back
@@ -45,29 +56,48 @@ const order = async (req, res) => {
 
 
     // validation of correct order format
-    const validationError = validateOrder(req.body)
-    if (validationError) return res.status(400).json({ message: validationError.details[0].message })
+    // const validationError = validateOrder(req.body)
+    // if (validationError) return res.status(400).json({ message: validationError.details[0].message })
 
 
     // destructure data
     let { items, address, slot } = req.body
 
-    try {
-        if (!items) throw new Error('Items not found!')
-        if (!address || typeof address != 'object') throw new Error('Address not found or not valid!')
 
-        slot = Date.parse(slot)
+    try {
+        // if (!items) throw new Error('Items not found!')
+        // if (!address || typeof address != 'object') throw new Error('Address not found or not valid!')
+
+        slot = Date.parse(slot || "2020-11-16T16:51:28.190Z")
         if (!slot) throw new Error("slot not valid!")
 
-        const totalPrice = items.reduce((a, c) => a + c.varieties[0].price * c.qty, 0)
+        // const totalPrice = items.reduce((a, c) => a + (c.varieties[0].price * ((1 - c.varieties[0].sale) / 100)) * c.qty, 0)
 
+        const razorPayOptions = {
+            amount: price,
+            currency: 'INR',
+            receipt: uuid(),
+            payment_capture: 0
+        }
+
+        const orderRz = await instance.orders.create(razorPayOptions)
         // finally everyting ok so add it to db.
-        // await user.updateOne({ $push: { $orders: req.body } })
-        const order = { items, address, slot, totalPrice }
-        console.log(order)
-        res.json({ message: "Order successful!" })
+        const d = await user.updateOne({ $push: { $orders: { totalPrice: price, razorPayId: orderRz.id, razorPayReceipt: orderRz.receipt, items, address } } })
+        console.log(d)
+        res.json(orderRz)
     } catch (error) {
         res.status(400).json({ message: error.message })
+    }
+}
+
+const captureOrder = async (req, res) => {
+    const url = `https://${process.env.RAZOR_PAY_KEY_ID}:${process.env.RAZOR_PAY_KEY_SECRET}@api.razorpay.com/v1/payments/${req.params.paymentId}/capture`
+
+    try {
+        const { data } = await Axios.post(url, { amount: 10 * 100, currency: 'INR' })
+        res.json(data)
+    } catch (err) {
+        res.status(500).json({ message: 'Something went wrong!' })
     }
 }
 
